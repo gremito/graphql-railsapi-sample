77ed73a04f9dd51eddef9b35adc11e4b12f78622
gremito <grem.takuya0223@gmail.com>
2020-11-26 00:15:59 +0900

    add post for create/update/delete

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,10 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+    field :delete_post, mutation: Mutations::DeletePost
+    field :update_post, mutation: Mutations::UpdatePost
+    field :createPost, mutation: Mutations::CreatePost
   end
 end
